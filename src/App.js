4ff18c4991dc0d3cbeb8a008a38789e85fdc148d
kogepee <78298838+kogepee@users.noreply.github.com>
2021-04-03 14:53:30 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import Form from './Form'
+import List from './List'
+import shortid from 'shortid'
+
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      content: '課題をする'
+    },
+    {
+      content: '洗濯をする'
+    },
+    {
+      content: '電話をする'
+    }
+  ])
+
+  const addTodo = content => {
+    setTodos([
+      ...todos,
+      {
+        content: content,
+        id: shortid.generate()
+      }
+    ])
+  }
+
+  const deleteTodo = id => {
+    setTodos(todos.filter(todo => todo.id !== id))
+}
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Form addTodo={addTodo} />
+      <List todos={todos} deleteTodo={deleteTodo}/>
+    </>
+  )
+}
+
+export default App
